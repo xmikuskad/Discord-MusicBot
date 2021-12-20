daef1783941d53276313b9f9cdc1a086f2c4a0c7
Dominik Mikuška <43302506+xmikuskad@users.noreply.github.com>
2021-12-20 21:38:27 +0100

    another lavalink server

--- botconfig.js
+++ botconfig.js
@@ -28,10 +28,10 @@ module.exports = {
   //Lavalink
   Lavalink: {
     id: "Main",
-    host: "disbotlistlavalink.ml",
+    host: "lavalink-priyam.ml",
     port: 443, // The port that lavalink is listening to. This must be a number!
-    pass: "LAVA",
-    secure: false,// Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
+    pass: "methisbigbrain",
+    secure: true,// Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
   },
 
   //Please go to https://developer.spotify.com/dashboard/
