ad4730a750a16258ea0e01395c20276fa8c0ad6f
Dominik Mikuška <43302506+xmikuskad@users.noreply.github.com>
2021-12-20 21:40:39 +0100

    added ww.

--- botconfig.js
+++ botconfig.js
@@ -28,7 +28,7 @@ module.exports = {
   //Lavalink
   Lavalink: {
     id: "Main",
-    host: "lavalink-priyam.ml",
+    host: "www.lavalink-priyam.ml",
     port: 443, // The port that lavalink is listening to. This must be a number!
     pass: "methisbigbrain",
     secure: true,// Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
